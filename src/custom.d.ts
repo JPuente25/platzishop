553ef8be91f5f0295827b505a0c245b8a8340fb7
Jaime Puente <jpcontreras28@gmail.com>
2023-01-27 09:25:05 -0400

    removed src and public

--- src/custom.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.svg' {
-   const ReactComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-   export default ReactComponent;
-}
--- src/pages/Home/index.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { MyOrders } from '../../containers/MyOrders';
-import { very_light_pink } from "../../styles/GlobalStyle";
-
-export const MyOrdersContainer = styled.div`
-   padding-left: 12px;
-   border-left: 1px solid ${very_light_pink};
-`;
